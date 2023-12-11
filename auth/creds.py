32877bc7b320182ad66ba1c51eeaaeb8d121ff80
Shmaryahu B <shbedev@users.noreply.github.com>
2023-12-11 10:51:26 +0200

    Update creds.py

--- auth/creds.py
+++ auth/creds.py
@@ -1,3 +1,3 @@
-instance_id = '' # Jamf Pro URL (e.g. Jamf Cloud == https://companyxyz.jamfcloud.com, On-Prem Jamf Pro == jamfpro.companyname.com:8443
+instance_id = '' # Jamf Pro URL (e.g. Jamf Cloud == companyname.jamfcloud.com, On-Prem Jamf Pro == jamfpro.companyname.com:8443
 username = ''    # API user that can be generated in Jamf Pro -> System Settings -> Jamf Pro User Accounts & Groups
 password = ''    # password for the above user
